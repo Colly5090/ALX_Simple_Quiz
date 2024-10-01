448c9611824aa1a71d3530c9186efcf2a138ebdc
Collins Boakye <colly0087@gmail.com>
2024-10-01 03:04:42 -0700

    making the quiz interactive and functional.

--- quiz.js
+++ quiz.js
@@ -1,18 +1,18 @@
 function checkAnswer() {
     let correctAnswer = '4';
-    let radios = document.querySelectorAll('input[name="quiz"]');
     let userAnswer;
+    let checkedRadio = document.querySelector('input[name="quiz"]');
+
+    if (checkedRadio.checked){
+        userAnswer = checkedRadio.value;
+    }
     const feedback = document.getElementById("feedback");
 
-    radios.forEach(radio => {
-        if (radio.checked){
-            userAnswer = radio.value;
-        }
-    });
     if (userAnswer === correctAnswer) {
         feedback.textContent = "Correct! Well done.";
-    }else {feedback.textContent = "That's incorrect. Try again!";}
-    
+    } else {
+        feedback.textContent = "That's incorrect. Try again!";
+    }
 }
 const button = document.getElementById("submit-answer");
 button.addEventListener('click', checkAnswer);
